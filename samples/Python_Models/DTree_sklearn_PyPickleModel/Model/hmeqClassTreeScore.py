dea531ce13b5289938c94d83d35909850b2bcd68
Kristen Aponte <Kristen.Aponte@sas.com>
2020-06-16 19:31:35 -0400

    Updated links to How-To Tutorial videos.

--- samples/Python_Models/DTree_sklearn_PyPickleModel/Model/hmeqClassTreeScore.py
+++ samples/Python_Models/DTree_sklearn_PyPickleModel/Model/hmeqClassTreeScore.py
@@ -1,94 +1,91 @@
-# Copyright (c) 2020, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-
-# %%
-# import libraries
 import math
-import numpy as np
-import pandas as pd
 import pickle
+import pandas as pd
+import numpy as np
 import settings
 
-# %%
-def scoreHMEQClassTreeModel(JOB, REASON, CLAGE, CLNO, DEBTINC, DELINQ, DEROG, NINQ):
+def scorehmeqClassTree(REASON, JOB, YOJ, DEROG, DELINQ, CLAGE, NINQ, CLNO, DEBTINC):
     "Output: EM_EVENTPROBABILITY, EM_CLASSIFICATION"
-    
+
     try:
         _thisModelFit
     except NameError:
-        with open(settings.pickle_path + "hmeqClassTree.pickle", "rb") as _pFile:
+        with open(settings.pickle_path + 'hmeqClassTree.pickle', 'rb') as _pFile:
             _thisModelFit = pickle.load(_pFile)
-            
-    # Threshold for the misclassification error (BAD: 0-No, 1-Yes)
-    _threshPredProb = 0.08941485864562787
 
-    # Impute the overall median for missing values
-    if (math.isnan(CLAGE)):
-        CLAGE = 173.46666666666600
+    try:
+        categoryStr = REASON.strip()
+    except AttributeError:
+        categoryStr = 'Other'
 
-    if (math.isnan(CLNO)):
-        CLNO = 20.0
+    REASON_HomeImp = np.where(categoryStr == 'HomeImp', 1.0, 0.0)
+    REASON_DebtCon = np.where(categoryStr == 'DebtCon', 1.0, 0.0)
 
-    if (math.isnan(DEBTINC)):
-        DEBTINC = 34.81826181858690
+    try:
+        categoryStr = JOB.strip()
+    except AttributeError:
+        categoryStr = 'Other'
 
-    # Impute the overall mode for missing values
-    if (math.isnan(DEROG)):
-        DEROG = 0.0
+    JOB_Other = np.where(categoryStr == 'Other', 1.0, 0.0)
+    JOB_Office = np.where(categoryStr == 'Office', 1.0, 0.0)
+    JOB_Sales = np.where(categoryStr == 'Sales', 1.0, 0.0)
+    JOB_Mgr = np.where(categoryStr == 'Mgr', 1.0, 0.0)
+    JOB_ProfExe = np.where(categoryStr == 'ProfExe', 1.0, 0.0)
+    JOB_Self = np.where(categoryStr == 'Self', 1.0, 0.0)
 
-    if (math.isnan(NINQ)):
-        NINQ = 0.0
+    try:
+        if math.isnan(YOJ):
+            YOJ = 8.922268135904499
+    except TypeError:
+        YOJ = 8.922268135904499
 
-    if (JOB == None):
-        cStr = "Other"
+    try:
+        if math.isnan(DEROG):
+            DEROG = 0.2545696877380046
+    except TypeError:
+        DEROG = 0.2545696877380046
+
+    try:
+        if math.isnan(DELINQ):
+            DELINQ = 0.4494423791821561
+    except TypeError:
+        DELINQ = 0.4494423791821561
+
+    try:
+        if math.isnan(CLAGE):
+            CLAGE = 179.76627518656605
+    except TypeError:
+        CLAGE = 179.76627518656605
+
+    try:
+        if math.isnan(NINQ):
+            NINQ = 1.1860550458715597
+    except TypeError:
+        NINQ = 1.1860550458715597
+
+    try:
+        if math.isnan(CLNO):
+            CLNO = 21.29609620076682
+    except TypeError:
+        CLNO = 21.29609620076682
+
+    try:
+        if math.isnan(DEBTINC):
+            DEBTINC = 33.77991534872112
+    except TypeError:
+        DEBTINC = 33.77991534872112
+
+    inputArray = pd.DataFrame([[1.0, YOJ, DEROG, DELINQ, CLAGE, NINQ, CLNO, DEBTINC, REASON_HomeImp, REASON_DebtCon, JOB_Other, JOB_Office, JOB_Sales, JOB_Mgr, JOB_ProfExe, JOB_Self]],
+                              columns = ['const', 'YOJ', 'DEROG', 'DELINQ', 'CLAGE', 'NINQ', 'CLNO', 'DEBTINC', 'REASON_HomeImp', 'REASON_DebtCon', 'JOB_Other', 'JOB_Office', 'JOB_Sales', 'JOB_Mgr', 'JOB_ProfExe', 'JOB_Self'],
+                              dtype = float)
+
+    prediction = _thisModelFit.predict(inputArray)
+
+    EM_EVENTPROBABILITY = float(prediction)
+
+    if (EM_EVENTPROBABILITY >= 0.199496644295302):
+        EM_CLASSIFICATION = '1'
     else:
-        cStr = JOB.strip()
-        if (not cStr):
-            cStr = "Other"
-
-    JOB_Mgr = np.where(cStr == "Mgr", 1.0, 0.0)
-    JOB_Office = np.where(cStr == "Office", 1.0, 0.0)
-    JOB_Other = np.where(cStr == "Other", 1.0, 0.0)
-    JOB_ProfExe = np.where(cStr == "ProfExe", 1.0, 0.0)
-    JOB_Sales = np.where(cStr == "Sales", 1.0, 0.0)
-    JOB_Self = np.where(cStr == "Self", 1.0, 0.0)
-
-    if (REASON == None):
-        cStr = "DebtCon"
-    else:
-        cStr = REASON.strip()
-        if (not cStr):
-            cStr = "DebtCon"
-
-    REASON_DebtCon = np.where(REASON == 'DebtCon', 1.0, 0.0)
-    REASON_HomeImp = np.where(REASON == 'HomeImp', 1.0, 0.0)
-
-    # Construct the input array for scoring (the first term is for the Intercept)
-    inputArray = pd.DataFrame([[JOB_Mgr, JOB_Office,
-                                 JOB_Other, JOB_ProfExe,
-                                 JOB_Sales, JOB_Self,
-                                 REASON_DebtCon, REASON_HomeImp,
-                                 CLAGE, CLNO, DEBTINC,
-                                 DELINQ, DEROG, NINQ]],
-                               columns = ["JOB_Mgr", "JOB_Office",
-                                          "JOB_Other", "JOB_ProfExe",
-                                          "JOB_Sales", "JOB_Self",
-                                          "REASON_DebtCon", "REASON_HomeImp",
-                                          "CLAGE", "CLNO", "DEBTINC",
-                                          "DELINQ", "DEROG", "NINQ"],
-                               dtype = float)
-
-    # Calculate the predicted probabilities
-    _predProb = _thisModelFit.predict_proba(inputArray)
-
-    # Retrieve the event probability
-    EM_EVENTPROBABILITY = float(_predProb[:,1])
-
-    # Determine the predicted target category
-    if (EM_EVENTPROBABILITY >= _threshPredProb):
-        EM_CLASSIFICATION = "1"
-    else:
-        EM_CLASSIFICATION = "0"
+        EM_CLASSIFICATION = '0' 
 
     return(EM_EVENTPROBABILITY, EM_CLASSIFICATION)
